3b02293356476c4699b583ff5a5583f13779d363
Ali Ranjbari <aliranjbari86@gmail.com>
2022-03-08 01:00:24 +0330

    complete sort_string

--- sort_string.c
+++ sort_string.c
@@ -5,13 +5,29 @@
 
 
 
+void
+sort_string(char* string){
+    int length = strlen(string);
+
+    for(int i=0; i<length; i++)
+        for(int j=i; j<length; j++)
+            if(string[i] > string[j]){
+                char temp = string[i];
+                string[i] = string[j];
+                string[j] = temp;
+            }
+    
+    printf(1, "%s\n", string);
+}
+
 
 int 
 main(int argc, char *argv[]){
-
     if(argc < 2){
         printf(1, "please enter a string\n");
     }
+    else
+        sort_string(argv[1]);
     exit();
 
 }
