0f743e30dd08b19de11f1fb5a577ced168dc200c
Ali Ranjbari <aliranjbari86@gmail.com>
2022-03-08 00:25:44 +0330

    create sort_string and add to MakeFile

---
+++ sort_string.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+
+
+
+int 
+main(int argc, char *argv[]){
+
+    if(argc < 2){
+        printf(1, "please enter a string\n");
+    }
+    exit();
+
+}
